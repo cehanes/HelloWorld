8250faead684243d1b8bcea2d490793f30fa3f33
cehanes <courtneyehanes@gmail.com>
2013-09-16 21:08:42 -0400

    Code files for Hello World programs

---
+++ HelloWorldC++.cpp
@@ -0,0 +1,17 @@
+// HelloWorldC++.cpp : Defines the entry point for the console application.
+//
+
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    cin.get();
+    return 0;
+}
+
---
+++ HelloWorldPython.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        HelloWorld Module
+# Purpose:
+#
+# Author:      Courtney Hanes
+#
+# Created:     09/02/2013
+# Copyright:   (c) Courtney Hanes 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+    print("Hello World!");
+
+
